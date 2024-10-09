c3b1372654d54d2167410e9c5bfd876195a63d5e
Jeimy Alejandra Rivera Duque <jeimyriveraduque@gmail.com>
2024-10-08 19:25:28 -0500

    Clase del Lunes

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-//asociar un evento al boton con el id: main_button
-let button = document.getElementById("main_button").addEventListener("click", ()=>{
-    alert("hola mundo")
+function Descuento(valor,porcentaje){
+    if (valor > 0 && porcentaje >0){
+        let descuento = valor - (valor * porcentaje / 100);
+        return descuento
+    }else{
+        return valor  
+    }
+}
+
+function multi(valor){
+    for (let i = 1; i <= 10; i++) {
+       console.log(valor+"*"+i+"= "+valor * i)  
+    }
+}
+
+document.getElementById("enviar").addEventListener("click", ()=> {
+    let input = document.getElementById("input").value
+    multi(input)
 })
+
+
---
+++ introduccion-js/app.js
@@ -0,0 +1,8 @@
+console.log("Hola mundo");
+let person = {
+    altura:1.57,
+    peso:34,
+    nombre:"juan"
+}
+
+console.table(person);
